bacf5251f3736292f55630b9dd4e068665223170
PritamKumar Ghosh <pritamkumar.ghosh@sita.aero>
2020-08-17 15:04:45 +0530

    jasper update

--- src/main/java/com/pns/contractmanagement/helper/impl/ContractInvoiceHelperImpl.java
+++ src/main/java/com/pns/contractmanagement/helper/impl/ContractInvoiceHelperImpl.java
@@ -5,6 +5,8 @@ import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
@@ -15,11 +17,13 @@ import com.pns.contractmanagement.model.Report;
 
 import net.sf.jasperreports.engine.JREmptyDataSource;
 import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JRExporterParameter;
 import net.sf.jasperreports.engine.JasperCompileManager;
 import net.sf.jasperreports.engine.JasperExportManager;
 import net.sf.jasperreports.engine.JasperFillManager;
 import net.sf.jasperreports.engine.JasperPrint;
 import net.sf.jasperreports.engine.JasperReport;
+import net.sf.jasperreports.engine.export.ooxml.JRDocxExporter;
 
 /**
  *
@@ -27,6 +31,7 @@ import net.sf.jasperreports.engine.JasperReport;
 @Component
 public class ContractInvoiceHelperImpl {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(ContractInvoiceHelperImpl.class);
     @Value("${app.print.date.formar:dd-MM-yyyy}")
     private String dateFormat;
 
@@ -45,12 +50,12 @@ public class ContractInvoiceHelperImpl {
 
             JasperPrint jasperPrint = JasperFillManager.fillReport(report, parameters, new JREmptyDataSource());
             byte[] pdfReport = JasperExportManager.exportReportToPdf(jasperPrint);
+
             //JasperExportManager.exportReportToPdfFile(jasperPrint, contract.getId() + ".pdf");
             return ImmutableReport.builder().content(pdfReport).fileName(contract.getId() + ".pdf")
                 .contentType(MediaType.APPLICATION_PDF).build();
-        } catch (JRException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
+        } catch (JRException ex) {
+            LOGGER.error("Exception occured while generatinf jasper report.", ex);
             return null;
         }
     }
