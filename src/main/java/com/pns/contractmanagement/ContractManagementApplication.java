1d7aa03c8ee1d9ede5e07e9ef0ff821e01da392c
PritamKumar Ghosh <pritamkumar.ghosh@sita.aero>
2020-08-19 13:13:23 +0530

    securrity - keycloak

--- src/main/java/com/pns/contractmanagement/ContractManagementApplication.java
+++ src/main/java/com/pns/contractmanagement/ContractManagementApplication.java
@@ -1,7 +1,9 @@
 package com.pns.contractmanagement;
 
+import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -16,4 +18,8 @@ public class ContractManagementApplication {
     }
     
 
+    @Bean
+    public KeycloakSpringBootConfigResolver keycloakConfigResolver() {
+        return new KeycloakSpringBootConfigResolver();
+    }
 }
